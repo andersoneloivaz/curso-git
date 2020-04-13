8153fa1c932ba2f638b5f5d5873052f8872e6550
Anderson Eloi Vaz <andersoneloivaz@gmail.com>
2020-04-13 16:56:13 -0300

    Declarada a variavel x

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -7,7 +7,7 @@ namespace projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            int x = 2;
         }
     }
 }
