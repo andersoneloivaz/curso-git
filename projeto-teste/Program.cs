f7ac3f3e32dae7c9c383cee25185b859ada9f7f7
Anderson Eloi Vaz <andersoneloivaz@gmail.com>
2020-04-13 17:19:16 -0300

    Declarado Y

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -8,11 +8,12 @@ namespace projeto_teste
         {
             Console.WriteLine("Hello World!");
             int x = 2;
-            
+            int y = 3;
 
             for (int i = 0; i < x; i++)
             {
                 Console.WriteLine(x);
+                Console.WriteLine(y);
 
             }
         }
